ce03b27ec92a6eafa17330d2507477d83c4ecd70
furkan karayel <furkan.karayel@outlook.de>
2024-08-08 20:26:27 +0200

    faster package to fetch data over grpc?

--- controllers/image.js
+++ controllers/image.js
@@ -1,68 +1,99 @@
-const { Model } = require("clarifai-nodejs");
-require('dotenv').config();
+const { ClarifaiStub, grpc } = require("clarifai-nodejs-grpc");
+require("dotenv").config();
 
-const PAT = process.env.PAT 
+const PAT = process.env.PAT;
 
 const handleFaceApiCall = async (req, res) => {
-    const modelUrl ="https://clarifai.com/clarifai/main/models/face-detection";
-    const detectorModel = new Model({
-    url: modelUrl,
-    authConfig: {
-        pat: PAT,
-    },
-    });
+  const USER_ID = "clarifai";
+  const APP_ID = "main";
+  const MODEL_ID = "face-detection";
 
-    try {
-        const result = await detectorModel
-            .predictByUrl({
-                url: req.body.input,
-                inputType: "image",
-            })
-            
-        res.json(result)
-    } catch (error) {
-        res.status(404).json('failed to fetch face detection api')
+  const stub = ClarifaiStub.grpc();
+
+  const metadata = new grpc.Metadata();
+  metadata.set("authorization", "Key " + PAT);
+
+  stub.PostModelOutputs(
+    {
+      user_app_id: {
+        user_id: USER_ID,
+        app_id: APP_ID,
+      },
+      model_id: MODEL_ID,
+      inputs: [
+        {
+          data: {
+            image: {
+              url: req.body.input,
+              allow_duplicate_url: true,
+            },
+          },
+        },
+      ],
+    },
+    metadata,
+    (err, response) => {
+      if (err) {
+        res.status(404).json("failed to fetch face detection api");
+      }
+      res.json(response)
     }
-}
+  );
+
+};
 
 const handleDescriptionApiCall = async (req, res) => {
-    const modelUrl = "https://clarifai.com/salesforce/blip/models/general-english-image-caption-blip"
-    const detectorModel = new Model({
-    url: modelUrl,
-    authConfig: {
-        pat: PAT,
-    },
-    });
+    const USER_ID = 'salesforce';
+    const APP_ID = 'blip';
+    const MODEL_ID = "general-english-image-caption-blip";
 
-    try {
-        const result = await detectorModel
-            .predictByUrl({
+    const stub = ClarifaiStub.grpc();
+
+    const metadata = new grpc.Metadata();
+    metadata.set("authorization", "Key " + PAT);
+
+    stub.PostModelOutputs(
+        {
+        user_app_id: {
+            user_id: USER_ID,
+            app_id: APP_ID,
+        },
+        model_id: MODEL_ID,
+        inputs: [
+            {
+            data: {
+                image: {
                 url: req.body.input,
-                inputType: "image",
-            })
-            
-        res.json(result)
-    } catch (error) {
-        res.status(404).json('failed to fetch description api')
-    }
-}
+                allow_duplicate_url: true,
+                },
+            },
+            },
+        ],
+        },
+        metadata,
+        (err, response) => {
+        if (err) {
+            res.status(404).json("failed to fetch description api");
+        }
+        res.json(response)
+        }
+    );
+};
 
-
-const handleImage = (db) => (req,res) => {
- 
-    const { id } = req.body;
-        db('users')
-        .where('id', id)
-        .increment('entries', 1)
-        .returning('entries')
-        .then(entries => {
-            res.json(entries[0].entries)
-        })
-        .catch(err => res.status(400).json('failed to get entries'))
-}
+const handleImage = (db) => (req, res) => {
+  const { id } = req.body;
+  db("users")
+    .where("id", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {
+      res.json(entries[0].entries);
+    })
+    .catch((err) => res.status(400).json("failed to get entries"));
+};
 
 module.exports = {
-    handleImage: handleImage,
-    handleFaceApiCall: handleFaceApiCall,
-    handleDescriptionApiCall: handleDescriptionApiCall
-}
+  handleImage: handleImage,
+  handleFaceApiCall: handleFaceApiCall,
+  handleDescriptionApiCall: handleDescriptionApiCall,
+};
